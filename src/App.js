be4a2c01a34a5bf837de6320c80246608cff573c
vishal-1230 <vishalvishwajeet422@gmail.com>
2023-07-18 13:47:50 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import items from './items.json';
 
-function App() {
+// Assume you have your JSON data in a variable called items.json
+
+
+const itemsData = JSON.parse(JSON.stringify(items));
+
+const App = () => {
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [quantity, setQuantity] = useState('');
+
+  const handleItemSelect = (item) => {
+    setSelectedItem(item);
+    setQuantity(''); // Reset quantity input when a new item is selected
+  };
+
+  const handleQuantityChange = (event) => {
+    setQuantity(event.target.value);
+  };
+
+  const calculateIngredientQuantities = () => {
+    if (!selectedItem || !quantity) {
+      return {};
+    }
+
+    const ingredientQuantities = {};
+    const multiplier = parseFloat(quantity) / parseFloat(selectedItem.unit);
+
+    for (const [ingredient, quantityPerUnit] of Object.entries(selectedItem.ingredients)) {
+      ingredientQuantities[ingredient] = quantityPerUnit * multiplier;
+    }
+
+    return ingredientQuantities;
+  };
+
+  const ingredientQuantities = calculateIngredientQuantities();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Select an item and enter quantity:</h1>
+      <div>
+        {itemsData.map((item, index) => (
+          <button key={index} onClick={() => handleItemSelect(item)}>
+            {item.title}
+          </button>
+        ))}
+      </div>
+      {selectedItem && (
+        <div>
+          <p>Selected Item: {selectedItem.title}</p>
+          <p>Unit: {selectedItem.unit}</p>
+          <input
+            type="number"
+            placeholder="Enter quantity"
+            value={quantity}
+            onChange={handleQuantityChange}
+          />
+          {quantity && (
+            <div>
+              <h2>Ingredient Quantities:</h2>
+              <ul>
+                {Object.entries(ingredientQuantities).map(([ingredient, quantity]) => (
+                  <li key={ingredient}>
+                    {ingredient}: {quantity} units
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
